241949e1bb03ae06f66dd23b56dfc771de5848ff
anandarahmawati24 <rahmawatiananda571@gmail.com>
2024-02-21 22:48:20 +0700

    fix

--- Jobsheet1/Array04.java
+++ Jobsheet1/Array04.java
@@ -1,89 +1,77 @@
 import java.util.Scanner;
 public class Array04 {
     public static void main(String[] args) {
-        String[] Matkul = { "Pancasila", 
-                              "Konsep Teknologi Informasi", 
-                              "Critical Thinking and Problem Solving", 
-                              "Matematika Dasar", 
-                              "Bahasa Inggris", 
-                              "Dasar Pemrograman", 
-                              "Praktikum Dasar Pemrograman", 
-                              "Keselamatan dan Kesehatan Kerja"};
-
+        String[] Matkul = { "Critical Thinking and Problem Solving", "Bahasa Inggris", "Pancasila", "Matematika Dasar",
+                "Konsep Teknologi Informasi", "Dasar Pemrograman", "Praktikum Dasar Pemrograman",
+                "Keselamatan dan Kesehatan Kerja" };
         double[] nilaiAngka = new double[Matkul.length];
         int totalSks = 0;
-        double totalBobot = 0;
+        double totBobot = 0;
         Scanner input04 = new Scanner(System.in);
         System.out.println("PROGRAM MENGHITUNG IP SEMESTER");
         System.out.println("=============================");
-        System.out.print("Masukkan jumlah SKS: ");
-        int sks = input04.nextInt();
-        for (int i = 0; i < Matkul.length && i < sks; i++) {
 
-            System.out.print("Masukkan nilai Angka untuk MK " + Matkul[i] + " : ");
+        for (int i = 0; i < Matkul.length; i++) {
+            System.out.print("Masukkan nilai (0-100) untuk Mata kuliah " + Matkul[i] + " : ");
             double nilai = input04.nextDouble();
             if (nilai >= 0 && nilai <= 100) {
                 double bobot = 0;
                 if (nilai >= 80) {
                     bobot = 4.0;
-                } else if (nilai >= 75) {
-                    bobot = 3.75;
-                } else if (nilai >= 70) {
+                } else if (nilai >= 73) {
                     bobot = 3.50;
                 } else if (nilai >= 65) {
                     bobot = 3.00;
                 } else if (nilai >= 60) {
                     bobot = 2.50;
-                } else if (nilai >= 55) {
-                    bobot = 2.00;
                 } else if (nilai >= 50) {
+                    bobot = 2.00;
+                } else if (nilai >= 39) {
                     bobot = 1.00;
                 }
                 nilaiAngka[i] = nilai;
-                totalBobot += bobot;
+                totBobot += bobot;
                 totalSks++;
             } else {
-                System.out.println("Nilai harus di antara 0 - 100");
+                System.out.println("nilai ");
                 return;
             }
         }
         System.out.printf("=======================%n");
-        System.out.printf("%n%nHASIL KONVERSI NILAI%n");
+        System.out.printf("%n%nHasil Konversi Nilai%n");
         System.out.printf("=======================%n");
         System.out.printf("%-40s%10s%15s%15s%n", "Mata Kuliah", "Nilai Angka", "Nilai Huruf", "Bobot Nilai");
 
         for (int i = 0; i < totalSks; i++) {
             double bobot = 0;
+            String NilaiHuruf;
             if (nilaiAngka[i] >= 80) {
                 bobot = 4.0;
-            } else if (nilaiAngka[i] >= 75) {
-                bobot = 3.75;
-            } else if (nilaiAngka[i] >= 70) {
+                NilaiHuruf = "A";
+            } else if (nilaiAngka[i] >= 73) {
                 bobot = 3.50;
+                NilaiHuruf = "B+";
             } else if (nilaiAngka[i] >= 65) {
                 bobot = 3.00;
+                NilaiHuruf = "B";
             } else if (nilaiAngka[i] >= 60) {
                 bobot = 2.50;
-            } else if (nilaiAngka[i] >= 55) {
-                bobot = 2.00;
+                NilaiHuruf = "C+";
             } else if (nilaiAngka[i] >= 50) {
+                bobot = 2.00;
+                NilaiHuruf = "C";
+            } else if (nilaiAngka[i] >= 39) {
                 bobot = 1.00;
+                NilaiHuruf = "D";
+            } else {
+                bobot = 0.00;
+                NilaiHuruf = "E";
             }
-            System.out.printf("%-40s%10s%15s%15s%n", Matkul[i], nilaiAngka[i], convertToNilaiHuruf(nilaiAngka[i]), bobot);
+            System.out.printf("%-40s%10s%15s%15s%n", Matkul[i], nilaiAngka[i], NilaiHuruf, bobot);
         }
-        System.out.printf("%nIP SEMESTER : %.2f%n", totalBobot / totalSks);
+        System.out.printf("IP SEMESTER : %.2f%n", totBobot / totalSks);
     }
-    private static String convertToNilaiHuruf(double nilaiSetara) {
-        if (nilaiSetara >= 3.75) {
-            return "A";
-        } else if (nilaiSetara >= 3.50) {
-            return "B+";
-        } else if (nilaiSetara >= 3.00) {
-            return "B";
-        } else if (nilaiSetara >= 2.50) {
-            return "C";
-        } else {
-            return "D";
-    }
-    }
-    }
+
+  
+
+}
