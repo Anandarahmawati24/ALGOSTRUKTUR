0512d76d766b12b379487caacd4936aeba5c0b80
anandarahmawati24 <rahmawatiananda571@gmail.com>
2024-03-17 21:35:17 +0700

    modif pertanyaan

--- Jobsheet4/Pangkat.java
+++ Jobsheet4/Pangkat.java
@@ -1,23 +1,29 @@
 package Jobsheet4;
-
 public class Pangkat {
-    public int nilai,pangkat;
+    public int nilai, pangkat;
+
+    public Pangkat(int n, int p){
+        nilai = n;
+        pangkat = p;
+    }
+
     int pangkatBF(int a, int n){
-        int hasil =1;
-        for(int i=0; i<n; i++){
-            hasil *= a;
+        int pangkat = 1;
+        for (int i=1; i<=n; i++){
+            pangkat *= a;
         }
-        return hasil;
+        return pangkat;
+
     }
-    int pangkatDC (int a, int n){
-        if (n==1){
-            return a;
-        } else{
-            if (n%2==0){
-                return pangkatDC(a,n/2)*pangkatDC(a,n/2)*a;
-            } else{
-                return pangkatDC(a,n/2)*pangkatDC(a,n/2);
-            }
+
+    int pangkatDC(int a, int n){
+        if (n==1)
+        return a;
+        else{
+            if(n%2==0)
+            return pangkatDC(a, n/2)*pangkatDC(a, n/2);
+            else
+            return pangkatDC(a, n/2)*pangkatDC(a, n/2)*a;
         }
     }
-    }
+}
