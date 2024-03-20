25ff0acf8e3a58363c6e483e4e334767a7e0d8c6
anandarahmawati24 <rahmawatiananda571@gmail.com>
2024-03-20 13:43:11 +0700

    modif percobaan

--- Jobsheet5/mainMahasiswa.java
+++ Jobsheet5/mainMahasiswa.java
@@ -21,5 +21,9 @@ public class mainMahasiswa {
         System.out.println("Data mahasiswa setelah sorting desc berdasarkan ipk");
         list.bubbleSort();
         list.tampil();
+
+        System.out.println("Data mahasiswa setelah sorting asc berdasarkan ipk");
+        list.selectionSort();
+        list.tampil();
 }
 }
