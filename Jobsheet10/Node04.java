dab37f8ea283fe33daf7065b36bc7b0c6f35f438
anandarahmawati24 <rahmawatiananda571@gmail.com>
2024-05-11 12:22:31 +0700

    prak 1

---
+++ Jobsheet10/Node04.java
@@ -0,0 +1,12 @@
+package Jobsheet10;
+
+public class Node04 {
+    int data;
+    Node04 prev, next;
+
+    Node04(Node04 prev, int data, Node04 next) {
+        this.prev=prev;
+        this.data= data;
+        this.next=next;
+    }
+}
